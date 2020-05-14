38eefe9d1952781d90ce3c96fc7ed294a7e7f104
Emre <icbm.emre@gmail.com>
2020-05-14 15:44:40 +0300

    test

--- src/main/java/com/example/demo/PlaygroundApplication.java
+++ src/main/java/com/example/demo/PlaygroundApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PlaygroundApplication {
@@ -9,5 +11,16 @@ public class PlaygroundApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlaygroundApplication.class, args);
 	}
+
+}
+
+@RestController("/")
+class SimpleResource {
+
+
+  @GetMapping("/hi")
+  public String testMethod(){
+	  return "Hello";
+  }
 
 }
--- src/test/java/com/example/demo/PlaygroundApplicationTests.java
+++ src/test/java/com/example/demo/PlaygroundApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 class PlaygroundApplicationTests {
@@ -11,7 +16,14 @@ class PlaygroundApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	
+
+	@Test
+    public void testMethod() {
+			String context="Hello";
+			assertEquals(context, "Hello");
+	   
+    }
+
 	@Test
 	void simpleTest(){
 	String context="Hello";
